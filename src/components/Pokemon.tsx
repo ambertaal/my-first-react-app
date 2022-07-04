e7f9f03f5789cb88cffe577887fa38eb77ec6df1
ambertaal <amber.taal@gmail.com>
2022-07-04 15:47:04 +0200

    add abilities amount and list

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -35,7 +35,12 @@ export default function Pokemon({
       <p>Weight: {weight} kg</p>
       <p>Awesome: {awesome ? "YES!" : "nope, not really"}</p>
       <p>Terrifying: {terrifying ? "Very" : "nah, lovable"}</p>
-      <p>Abilities: {abilities}</p>
+      <p>Abilities ({abilities.length}):</p>
+      <ul>
+        {abilities.map((ability) => {
+          return <li>{ability}</li>;
+        })}
+      </ul>
     </div>
   );
 }
